31625f0ddfd70d84730ef8c9b4fee93734421b64
ipramudya <ipramudya.dev@gmail.com>
2023-01-08 01:40:53 +0700

    build: blank page #4

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { useUserStore } from '@/stores/user.store';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
