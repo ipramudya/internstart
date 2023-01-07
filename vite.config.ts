0a47de40bf5f0626db6ff3cefa74b66bbcff9286
ipramudya <ipramudya.dev@gmail.com>
2023-01-08 00:59:58 +0700

    build: blank page #3

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
-
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [vue()],
-    server: {
-        port: 3000,
-    },
-    base: './',
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@': path.resolve(__dirname, 'src'),
         },
     },
+    base: './',
 });
